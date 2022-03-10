47ffc8192864b89ff8d3600c0e8749d05e094053
Re-taro <aminokacho@gmail.com>
2022-03-10 22:03:55 +0900

    ✨ update to v1.0.6

--- nest.js
+++ nest.js
@@ -4,16 +4,12 @@ module.exports = {
     node: true
   },
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
-  overrides: [
-    {
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
-      }
-    }
-  ],
   parser: ['@typescript-eslint/parser'],
-  plugins: ['@typescript-eslint/eslint-plugin']
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 }
